4728fb2def23d7ac869e2a39a0b8cfd70cbb9a53
Hacendado <elianbrionesvictores65@gmail.com>
2021-05-05 19:18:45 +0200

    Version 3.0

--- js/Slider.js
+++ js/Slider.js
@@ -1,1 +1,20 @@
-const slider_new = document.querySelector('.new');
+// Slider
+const sliderNews = document.getElementsByClassName("news");
+const sliderImages = document.getElementsByTagName("li");
+
+// Buttons
+const prevButton = document.getElementById("prevButton");
+const nextButton = document.getElementById("nextButton");
+
+// Counter
+let counter = 1;
+//const size = sliderImages[0].clientWidth;
+
+//sliderNews.style.transform = "translateX(-400px)";
+
+//Button Listeners
+nextButton.addEventListener("click", () => {
+    //sliderNews.style.transition = "all 0.4s ease-in-out";
+    counter++;
+    sliderNews.style.transform = "translateX(-400px)";
+});
